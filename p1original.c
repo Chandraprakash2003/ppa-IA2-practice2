319228804469d021c4772485989aa188f879cf3a
Chandraprakas17 <96509306+Chandraprakash2003@users.noreply.github.com>
2022-02-13 11:20:06 +0000

    1st assignment done

--- p1original.c
+++ p1original.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 
-
+void input(float *base, float *height);
+void find_area(float base , float height, float *area);
+void output(float base, float height, float area);
 
 int main() {
   float b, h, ans;
